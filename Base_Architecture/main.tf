295a720987b8495ec25ec9a49438e2cccdea66bd
Samuel Udoh <samudoh93@gmail.com>
2017-11-19 19:58:46 +0000

    finished vpc setup

--- Base_Architecture/main.tf
+++ Base_Architecture/main.tf
@@ -7,17 +7,104 @@ provider "aws"{
 S3_access
 
 #VPC
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.1.0.0/16"
+
+}
+
 #Internet Gateway
+resource "aws_internet_gateway" "internet_gateway"{
+  vpc_id = "${aws_vpc.vpc.id}"
+}
 #Public route table
+resource "aws_route_table" "public"{
+  vpc_id = "${aws_vpc.vpc.id}"
+  route {
+            cidr_block = "0.0.0.0/0"
+            gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+          }
+  tags {
+          Name = "public"
+        }
+}
 #Private route
+resource "aws_default_route_table" "private" {
+  default_route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+  tags {
+      Name = "private"
+  }
+}
 #Subnets
+resource "aws_subnet" "public" {
+  vpc_id ="${aws_vpc.vpc.id}"
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-1d"
+
+  tags {
+      Name = "public"
+  }
+}
 #public Subnets
 #private 1
-#private 2
-#RDS1
-#RDS2
-#RDS3
 
+resource "aws_subnet" "private1" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "eu-west-1b"
+
+  tags {
+      Name = "private1"
+  }
+}
+
+
+#private 2
+resource "aws_subnet" "private2" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.1.3.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "eu-west-1c"
+
+  tags {
+      Name = "private2"
+  }
+}
+#RDS1
+resource "aws_subnet" "rds1" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.1.4.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "eu-west-1a"
+
+  tags {
+      Name = "rds1"
+  }
+}
+#RDS2
+resource "aws_subnet" "rds2" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.1.5.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "eu-west-1c"
+
+  tags {
+      Name = "rds2"
+  }
+}
+#RDS3
+resource "aws_subnet" "rds3" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.1.6.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "eu-west-1d"
+
+  tags {
+      Name = "rds3"
+  }
+}
 #Secruity groups
 
 #Private
