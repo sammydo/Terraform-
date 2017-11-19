dc155f2f7d3fc2c622b05a8a0d1f3338c5dcae7d
Samuel Udoh <samudoh93@gmail.com>
2017-11-19 01:15:53 +0000

    added template of project to main.tf

--- Base_Architecture/main.tf
+++ Base_Architecture/main.tf
@@ -2,3 +2,41 @@ provider "aws"{
   region = "${var.aws_region}"
   profile = "${var.aws_profile}"
 }
+
+#IAM
+S3_access
+
+#VPC
+#Internet Gateway
+#Public route table
+#Private route
+#Subnets
+#public Subnets
+#private 1
+#private 2
+#RDS1
+#RDS2
+#RDS3
+
+#Secruity groups
+
+#Private
+#Public
+#RDS
+
+#S3 Code bucket
+
+#computer
+#key pair
+#dev server
+  #ansible play
+#load balancer
+#AMI
+#launch
+#ASG
+
+#Route53
+#primary zone
+#www
+#dev
+#db
